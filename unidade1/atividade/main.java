1b114c94b299d28164e92c10acbc83680c84724d
Fabricio2300 <7fabriciolima777777@gmail.com>
2024-08-18 22:31:36 -0300

    Update main.java

---
+++ unidade1/atividade/main.java
@@ -0,0 +1,35 @@
+package br.com.ap3.distancia;
+
+import java.util.Scanner;
+
+public class Questao1 {
+
+    public static double getDistancia() {
+        Scanner teclado = new Scanner(System.in);
+
+        double x1, y1, x2, y2, distancia;
+
+        System.out.println("Entre com as coordenadas x e y dos pontos nesta ordem:");
+        x1 = teclado.nextDouble();
+        y1 = teclado.nextDouble();
+        x2 = teclado.nextDouble();
+
+        distancia = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        System.out.println("A distância é: " + distancia);
+        return distancia;
+    }
+
+    public static double getAmplitude() {
+        double val1 = 0.0, val2 = 1.0, val3 = -1.0;
+
+        double amplitude = Math.max(val1, Math.max(val2, val3)) - Math.min(val1, Math.min(val2, val3));
+
+        return amplitude;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Distância: " + getDistancia());
+        System.out.println("Amplitude: " + getAmplitude());
+    }
+}
